333a72d6ca905bb95a423f04ce98eb999fc690d7
Ken Tronnes <ken.tronnes@gmail.com>
2018-05-31 09:46:00 -0700

    adding more chapter 8 exercises

--- python_work/Python Crash Course Chapter 8/misc Chapter 8.py
+++ python_work/Python Crash Course Chapter 8/misc Chapter 8.py
@@ -168,27 +168,43 @@
 # show_completed_models(completed_models)
 
 
-def print_models(unprinted_designs, completed_models):
-	"""
-	Simulate printing each design, until none are left
-	Move each design to completed_models after printing.
-	"""
-	while unprinted_designs:
-		current_design = unprinted_designs.pop()
+# def print_models(unprinted_designs, completed_models):
+# 	"""
+# 	Simulate printing each design, until none are left
+# 	Move each design to completed_models after printing.
+# 	"""
+# 	while unprinted_designs:
+# 		current_design = unprinted_designs.pop()
 
-		# Simulate creating a 3D print from the design.
-		print("Printing: " + current_design)
-		completed_models.append(current_design)
+# 		# Simulate creating a 3D print from the design.
+# 		print("Printing: " + current_design)
+# 		completed_models.append(current_design)
 
-def show_completed_models(completed_models):
-	"""Show all the models that were printed."""
-	print("\nThe following models have been printed:")
-	for completed_model in completed_models:
-		print(completed_models)
+# def show_completed_models(completed_models):
+# 	"""Show all the models that were printed."""
+# 	print("\nThe following models have been printed:")
+# 	for completed_model in completed_models:
+# 		print(completed_models)
 
-unprinted_designs = ['ipnone case', 'robot pendant', 'dodecahedron']
-completed_models = []
+# unprinted_designs = ['ipnone case', 'robot pendant', 'dodecahedron']
+# completed_models = []
 
-print_models(unprinted_designs, completed_models)
-show_completed_models(completed_models)		
+# print_models(unprinted_designs, completed_models)
+# show_completed_models(completed_models)		
+
+# def make_pizza(*toppings):
+# 	"""Print the list of toppings that have been requested."""
+# 	print(toppings)
+
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')	
+
+def make_pizza(*toppings):
+	"""Summarize the pizza we are about to make."""
+	print("\nMaking a pizza with the following toppings:")
+	for topping in toppings:
+		print("- " + topping)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
